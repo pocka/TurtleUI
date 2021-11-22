2782f66bb8060af3f81ae8212779a4eb47bcc1fa
pocka <pocka@users.noreply.github.com>
2021-11-22 08:12:45 +0000

    deploy: 3f0c009b8774be82b2289083e01e4f39b028b13d

--- assets/spec.b0a154d1.js
+++ assets/spec.b0a154d1.js
@@ -93,7 +93,16 @@ Otherwise the visual appearance won&#39;t update.</p>
 `},{name:"--turtle-ui--color--tone--warning",description:`<p>H, S%</p>
 `},{name:"--turtle-ui--color--tone--danger",description:`<p>H, S%</p>
 `},{name:"--turtle-ui--color--tone--primary",description:`<p>H, S%</p>
-`}]},{name:"turtle-textbox",attributes:[{name:"touched",type:"boolean",default:"false"},{name:"novalidity",type:"boolean",default:"false"}],properties:[{name:"touched",attribute:"touched",type:"boolean",default:"false"},{name:"noValidity",attribute:"novalidity",type:"boolean",default:"false"},{name:"isValid",type:"boolean"},{name:"touch",type:"() => void",default:'"() => {\\n    if (!this.touched) {\\n      this.touched = true;\\n      this.dispatchEvent(new CustomEvent(\\"fieldtouch\\"));\\n    }\\n  }"'}],events:[{name:"validitychange"},{name:"fieldtouch"}]},{name:"turtle-form-field",description:`<p>A distinguishable property of a form.</p>
+`}]},{name:"turtle-textbox",description:`<p>Textbox.</p>
+`,attributes:[{name:"touched",description:`<p>Whether the user interacted to the slotted item.</p>
+`,type:"boolean",default:"false"},{name:"novalidity",description:`<p>When this attribute or property is set to <code>true</code>, the component goes into neither invalid nor valid state.</p>
+`,type:"boolean",default:"false"}],properties:[{name:"touched",attribute:"touched",description:`<p>Whether the user interacted to the slotted item.</p>
+`,type:"boolean",default:"false"},{name:"noValidity",attribute:"novalidity",description:`<p>When this attribute or property is set to <code>true</code>, the component goes into neither invalid nor valid state.</p>
+`,type:"boolean",default:"false"},{name:"isValid",description:`<p>Whether the slotted input is in valid state.</p>
+`,type:"boolean"}],events:[{name:"validitychange",description:`<p>Validity state changed.</p>
+`},{name:"fieldtouch",description:`<p>The user interacted the slotted input first time.</p>
+`}],slots:[{name:"",description:`<p><code>&lt;input&gt;</code>, except <code>type=&quot;radio&quot;</code> and <code>type=&quot;checkbox&quot;</code> at least.</p>
+`}]},{name:"turtle-form-field",description:`<p>A distinguishable property of a form.</p>
 <p>If you provide <code>&lt;turtle-textbox&gt;</code> or <code>&lt;turtle-selectbox&gt;</code> for the default slot and text element for the <code>description</code> slot,
 a color of the <code>description</code> slot will change depends on the validity state of the slotted <code>&lt;turtle-textbox&gt;</code> (or <code>&lt;turtle-selectbox&gt;</code>).</p>
 `,properties:[],slots:[{name:"",description:`<p>Form item, required.</p>
@@ -122,7 +131,8 @@ You need to provide an Element to slot, instead of a TextFragment.</p>
 `,default:'"8px"'}]},{name:"turtle-list",description:`<p>A list of actionable items.</p>
 `,properties:[],slots:[{name:"",description:`<p>One or more of <code>&lt;turtle-list-item&gt;</code>.</p>
 `}],cssProperties:[{name:"--turtle-list--radius",description:`<p>Corner radius.</p>
-`,default:'"8px"'}]},{name:"turtle-notification-action",attributes:[{name:"disabled",description:`<p>Whether the element itself is disabled or not.</p>
+`,default:'"8px"'}]},{name:"turtle-notification-action",description:`<p>An action button for <code>&lt;turtle-notification&gt;</code>.</p>
+`,attributes:[{name:"disabled",description:`<p>Whether the element itself is disabled or not.</p>
 <p>When the <code>lightDOM</code> property is <code>true</code>, this property does not affect to anything.
 Specify <code>disabled</code> or <code>aria-disabled</code> attribute for the slotted element (e.g. <code>&lt;button disabled/&gt;</code>, <code>&lt;a aria-disabled=&quot;true&quot;/&gt;</code>).</p>
 `,type:"boolean",default:"false"},{name:"lightdom",description:`<p>Whether to use a slotted element as a button element.
@@ -132,7 +142,21 @@ You need to provide an Element to slot, instead of a TextFragment.</p>
 Specify <code>disabled</code> or <code>aria-disabled</code> attribute for the slotted element (e.g. <code>&lt;button disabled/&gt;</code>, <code>&lt;a aria-disabled=&quot;true&quot;/&gt;</code>).</p>
 `,type:"boolean",default:"false"},{name:"lightDOM",attribute:"lightdom",description:`<p>Whether to use a slotted element as a button element.
 You need to provide an Element to slot, instead of a TextFragment.</p>
-`,type:"boolean",default:"false"}]},{name:"turtle-notification",properties:[]},{name:"turtle-radio-button",properties:[]},{name:"turtle-selectbox",attributes:[{name:"touched",type:"boolean",default:"false"},{name:"novalidity",type:"boolean",default:"false"}],properties:[{name:"touched",attribute:"touched",type:"boolean",default:"false"},{name:"noValidity",attribute:"novalidity",type:"boolean",default:"false"},{name:"touch",type:"() => void",default:'"() => {\\n    this.touched = true;\\n  }"'}]},{name:"turtle-snackbar-action",attributes:[{name:"disabled",description:`<p>Whether the element itself is disabled or not.</p>
+`,type:"boolean",default:"false"}],slots:[{name:"",description:`<p>Button-like element if <code>lightdom</code> is <code>true</code>, TextFragment otherwise.</p>
+`}]},{name:"turtle-notification",description:`<p>Notification card with user actions. Use when you want a user to perform an action, but don&#39;t want to interrupt the user.</p>
+`,properties:[],slots:[{name:"",description:`<p>Body texts.</p>
+`},{name:"image",description:`<p>Thumbnail <code>&lt;img&gt;</code> or icon.</p>
+`},{name:"action",description:`<p>An action related to the content. Up to 2.</p>
+`}],cssProperties:[{name:"--turtle-notifiaction--radius",description:`<p>Corner radius size.</p>
+`,default:'"8px"'}]},{name:"turtle-radio-button",description:`<p>Radio button.</p>
+`,properties:[],slots:[{name:"",description:`<p><code>&lt;input type=&quot;radio&quot;&gt;</code></p>
+`}]},{name:"turtle-selectbox",description:`<p>Select box.</p>
+`,attributes:[{name:"touched",description:`<p>Whether the user interacted to the slotted item.</p>
+`,type:"boolean",default:"false"},{name:"novalidity",description:`<p>When this attribute or property is set to <code>true</code>, the component goes into neither invalid nor valid state.</p>
+`,type:"boolean",default:"false"}],properties:[{name:"touched",attribute:"touched",description:`<p>Whether the user interacted to the slotted item.</p>
+`,type:"boolean",default:"false"},{name:"noValidity",attribute:"novalidity",description:`<p>When this attribute or property is set to <code>true</code>, the component goes into neither invalid nor valid state.</p>
+`,type:"boolean",default:"false"}],slots:[{name:"",description:`<p><code>&lt;select&gt;</code>.</p>
+`}]},{name:"turtle-snackbar-action",attributes:[{name:"disabled",description:`<p>Whether the element itself is disabled or not.</p>
 <p>When the <code>lightDOM</code> property is <code>true</code>, this property does not affect to anything.
 Specify <code>disabled</code> or <code>aria-disabled</code> attribute for the slotted element (e.g. <code>&lt;button disabled/&gt;</code>, <code>&lt;a aria-disabled=&quot;true&quot;/&gt;</code>).</p>
 `,type:"boolean",default:"false"},{name:"lightdom",description:`<p>Whether to use a slotted element as a button element.
@@ -142,11 +166,20 @@ You need to provide an Element to slot, instead of a TextFragment.</p>
 Specify <code>disabled</code> or <code>aria-disabled</code> attribute for the slotted element (e.g. <code>&lt;button disabled/&gt;</code>, <code>&lt;a aria-disabled=&quot;true&quot;/&gt;</code>).</p>
 `,type:"boolean",default:"false"},{name:"lightDOM",attribute:"lightdom",description:`<p>Whether to use a slotted element as a button element.
 You need to provide an Element to slot, instead of a TextFragment.</p>
-`,type:"boolean",default:"false"}]},{name:"turtle-snackbar",attributes:[{name:"severity",type:"Severity",default:'"info"'},{name:"lifetime",description:`<p>in seconds</p>
-`,type:"number | undefined"},{name:"lifetime-label",type:"string | undefined"},{name:"autofocus",type:"boolean",default:"false"}],properties:[{name:"severity",attribute:"severity",type:"Severity",default:'"info"'},{name:"lifetime",attribute:"lifetime",description:`<p>in seconds</p>
-`,type:"number | undefined"},{name:"lifetimeLabel",attribute:"lifetime-label",type:"string | undefined"},{name:"autofocus",attribute:"autofocus",type:"boolean",default:"false"}],events:[{name:"timerend"}]},{name:"turtle-toggle-switch",description:`<p>Toggle Switch UI for representing an on/off value.
-This component only works with Light DOM approach: you need to provide <code>&lt;input type=&quot;checkbox&quot;&gt;</code>
-and set properties on them, not this component.</p>
-<p>NOTE: Due to browsers fire neither <code>blur</code> nor <code>change/input</code> event on unchecked radio (probably the spec defined), you can&#39;t place <code>&lt;input type=&quot;radio&quot;&gt;</code>.</p>
-`,properties:[],slots:[{name:"",description:`<p>Slot for an <code>&lt;input type=&quot;checkbox&quot; /&gt;</code>.</p>
+`,type:"boolean",default:"false"}],slots:[{name:"",description:`<p>Button-like element if <code>lightdom</code> is set to <code>true</code>, otherwise <code>TextFragment</code>.</p>
+`}]},{name:"turtle-snackbar",description:`<p>Snackbar component, which delivers an uninterreputible message.</p>
+`,attributes:[{name:"severity",description:`<p>Severity of the message.</p>
+`,type:'"info" | "success" | "warning" | "danger"',default:'"info"'},{name:"lifetime",description:`<p>Countdown duration in seconds. When not present, neither countdown nor event dispatching starts.</p>
+`,type:"number | undefined"},{name:"lifetime-label",description:`<p>Label text for the countdown graphic. Recommended when you set <code>lifetime</code> attribute.</p>
+`,type:"string | undefined"},{name:"autofocus",description:`<p>Whether to recieve a focus when the element appears in the DOM tree.
+Same to native <code>autofocus</code> attribute.</p>
+`,type:"boolean",default:"false"}],properties:[{name:"severity",attribute:"severity",description:`<p>Severity of the message.</p>
+`,type:'"info" | "success" | "warning" | "danger"',default:'"info"'},{name:"lifetime",attribute:"lifetime",description:`<p>Countdown duration in seconds. When not present, neither countdown nor event dispatching starts.</p>
+`,type:"number | undefined"},{name:"lifetimeLabel",attribute:"lifetime-label",description:`<p>Label text for the countdown graphic. Recommended when you set <code>lifetime</code> attribute.</p>
+`,type:"string | undefined"},{name:"autofocus",attribute:"autofocus",description:`<p>Whether to recieve a focus when the element appears in the DOM tree.
+Same to native <code>autofocus</code> attribute.</p>
+`,type:"boolean",default:"false"}],events:[{name:"timerend",description:`<p>Countdown ended.</p>
+`}]},{name:"turtle-toggle-switch",description:`<p>Toggle Switch UI for representing an on/off value.</p>
+<p><strong>NOTE:</strong> Due to browsers fire neither <code>blur</code> nor <code>change/input</code> event on unchecked radio (probably the spec defined), you can&#39;t place <code>&lt;input type=&quot;radio&quot;&gt;</code>.</p>
+`,properties:[],slots:[{name:"",description:`<p><code>&lt;input type=&quot;checkbox&quot; /&gt;</code>.</p>
 `}]}];var o={version:e,tags:t};export{o as default,t as tags,e as version};
